e6d75cde55d1949c25b734cb8be1b63fe0d7a731
datnt <khodatabase@gmail.com>
2013-01-12 16:32:17 +0700

    add relationship for baitap and phepCong

--- app/models/bai_tap.rb
+++ app/models/bai_tap.rb
@@ -1,3 +1,6 @@
 class BaiTap < ActiveRecord::Base
   attr_accessible :description, :name
+
+  has_many :phep_cong
+  
 end
--- app/models/phep_cong.rb
+++ app/models/phep_cong.rb
@@ -1,3 +1,4 @@
 class PhepCong < ActiveRecord::Base
   attr_accessible :ketqua, :so_a, :so_b
+  belongs_to :bai_tap
 end
