22684c8366a876a23207f6295ccf1bbcefd71e36
datnt <khodatabase@gmail.com>
2013-02-12 15:34:51 +0700

    tao combobox cho loai cau hoi

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,3 @@
 class Category < ActiveRecord::Base
-  attr_accessible :ten
+  attr_accessible :ten, :ma_cau_hoi
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130212021949) do
+ActiveRecord::Schema.define(:version => 20130212082813) do
 
   create_table "bai_taps", :force => true do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "ten"
+    t.string   "ma_cau_hoi"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "chia_phansos", :force => true do |t|
